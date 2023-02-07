ac0d7d54e694b1e1a0dce3a55346c4e67529cee7
Meesterdaaf <d.vanoudheusden@rocva.nl>
2023-02-07 22:59:08 +0100

    init commit

---
+++ app/Http/Controllers/CalculateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculateController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        return view('form', $data);
+    }
+}
